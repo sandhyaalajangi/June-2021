18751f0c90a14e36fa10c91c40fad1eb7b14aee2
sandhyaalajangi <86951646+sandhyaalajangi@users.noreply.github.com>
2021-07-12 19:44:27 +1200

    Add files via upload

---
+++ wait.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace june2021.Utilities
+{
+    class wait
+    {
+        //reusauable funtion for wait
+        public static void WaitforWebElementToExist(IWebDriver driver, string attributeValue, string attribute,int secondsToWait)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, secondsToWait));
+            if (attribute == "XPath")
+            {
+                
+                IWebElement webElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(attributeValue)));
+
+            }
+
+            if (attribute == "ID")
+            {
+                
+                IWebElement webElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id(attributeValue)));
+
+            }
+
+
+            if (attribute == "CssSelector")
+            {
+               
+                IWebElement webElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.CssSelector(attributeValue)));
+
+            }
+        }
+    }
+}
